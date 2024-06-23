d2f3e16f0c9e6621b244a7b750756708b1a1c045
Adriano Stankewicz <adriano.stankewicz@gmail.com>
2024-06-23 10:32:02 -0300

    build: add dist path

---
+++ server/dist/adapters/nodemailer/nodemailer-adapter.js
@@ -0,0 +1,26 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.NodemailerMailAdapter = void 0;
+const nodemailer_1 = __importDefault(require("nodemailer"));
+const transport = nodemailer_1.default.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+        user: "c2bb1f0f61c04f",
+        pass: "c27575a46713cd"
+    }
+});
+class NodemailerMailAdapter {
+    async sendMail({ subject, body }) {
+        await transport.sendMail({
+            from: 'Equipe Feedget <oi@feedget.com>',
+            to: 'Adriano Stankewicz <adriano.stankewicz@gmail.com>',
+            subject: subject,
+            html: body
+        });
+    }
+}
+exports.NodemailerMailAdapter = NodemailerMailAdapter;
